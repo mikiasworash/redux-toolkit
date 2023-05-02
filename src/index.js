16c4c0f3342ba273d0c4520e0bd5fe2b1a1814b0
mikias-worash <mikias.worash.a@gmail.com>
2023-05-02 08:58:07 +0300

    setup store and create slice

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+// import store and provider
+import { store } from './store'
+import { Provider } from 'react-redux'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById('root')
+const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}></Provider>
     <App />
   </React.StrictMode>
-);
+)
